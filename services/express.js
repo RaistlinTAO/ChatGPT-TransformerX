c6d97ada9fec56adba8d249724c45841654dd487
Raistlin TAO <demonvk@gmail.com>
2023-02-05 21:26:04 +1100

    TRAN-2 Assets and Express

--- services/express.js
+++ services/express.js
@@ -6,17 +6,17 @@ import Routes from "../routes/index.js";
 class _ExpressService {
     initialBackend() {
         let app = express();
-        // app.set('views', path.join(path.resolve("./"), '/views'));
-        // app.set('view engine', 'ejs');
-        // app.use(session({
-        //     secret: 'XWJ', resave: false, saveUninitialized: true, cookie: {
-        //         //secure: false, httpOnly: true, expires: new Date(Date.now() + 2 * 60 * 60 * 1000)
-        //         secure: false, httpOnly: true, maxAge: 2 * 60 * 60 * 1000
-        //     }
-        // }))
+        app.set('views', path.join(path.resolve("./"), '/views'));
+        app.set('view engine', 'ejs');
+        app.use(session({
+            secret: 'GPT', resave: false, saveUninitialized: true, cookie: {
+                //secure: false, httpOnly: true, expires: new Date(Date.now() + 2 * 60 * 60 * 1000)
+                secure: false, httpOnly: true, maxAge: 2 * 60 * 60 * 1000
+            }
+        }))
         app.use(express.json());
         app.use(express.urlencoded({extended: false}));
-        // app.use("/", express.static(path.join(path.resolve("./"), '/public/assets')));
+        app.use("/", express.static(path.join(path.resolve("./"), '/public/assets')));
         Routes.setRoutes(app);
         let server = app.listen(process.env.SERVER_BACKEND_PORT);
         Utils.Logger.printInfo('Safety Controller Backend Service Started at: ' + 'http://localhost:' + process.env.SERVER_BACKEND_PORT);
