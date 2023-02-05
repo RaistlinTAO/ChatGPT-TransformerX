bdc1b2306e5e782c460e08a3d63ff0c194e21cf5
Raistlin TAO <demonvk@gmail.com>
2023-02-05 22:03:45 +1100

    TRAN-3 Landing page

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,11 @@ let homeRouter = express.Router();
 
 /* GET home page. */
 homeRouter.get('/', function (req, res, next) {
-    res.send('OK');
+    res.render('landing.ejs', {});
+});
+
+homeRouter.get('/generate', function (req, res, next) {
+    res.render('generator.ejs', {});
 });
 
 export default homeRouter;
--- services/express.js
+++ services/express.js
@@ -1,22 +1,16 @@
 import express from "express";
 import Utils from "../utils/index.js";
 import Routes from "../routes/index.js";
-
+import path from "path";
 
 class _ExpressService {
     initialBackend() {
         let app = express();
         app.set('views', path.join(path.resolve("./"), '/views'));
         app.set('view engine', 'ejs');
-        app.use(session({
-            secret: 'GPT', resave: false, saveUninitialized: true, cookie: {
-                //secure: false, httpOnly: true, expires: new Date(Date.now() + 2 * 60 * 60 * 1000)
-                secure: false, httpOnly: true, maxAge: 2 * 60 * 60 * 1000
-            }
-        }))
         app.use(express.json());
         app.use(express.urlencoded({extended: false}));
-        app.use("/", express.static(path.join(path.resolve("./"), '/public/assets')));
+        app.use("/", express.static(path.join(path.resolve("./"), '/public')));
         Routes.setRoutes(app);
         let server = app.listen(process.env.SERVER_BACKEND_PORT);
         Utils.Logger.printInfo('Safety Controller Backend Service Started at: ' + 'http://localhost:' + process.env.SERVER_BACKEND_PORT);
