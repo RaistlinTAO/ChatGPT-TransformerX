a0b2d7a2938d4a2325c15ce22c632c22aa5c98b3
Raistlin TAO <demonvk@gmail.com>
2023-02-06 03:39:53 +1100

    TRAN-5 Connected to ChatGPT api

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import {Configuration, OpenAIApi} from "openai";
 
 let homeRouter = express.Router();
 
@@ -12,8 +12,24 @@ homeRouter.get('/generate', function (req, res, next) {
     res.render('generator.ejs', {});
 });
 
-homeRouter.post('/transform', function (req, res, next) {
-    req.session.resultStr = "RESULT FROM CHAT GPT";
+homeRouter.post('/transform', async function (req, res, next) {
+    //req.body.keywords
+
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    let response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.keywords,
+        temperature: 0,
+        max_tokens: 3900,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+    console.log(response)
+    req.session.resultStr = response.data.choices[0].text;
     res.redirect('/result');
 });
 
