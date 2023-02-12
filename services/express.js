49f7f4f77f10264a398bda197dd54c58d4a1d4d5
Raistlin TAO <demonvk@gmail.com>
2023-02-13 03:55:42 +1100

    TRAN-7 Express Security Measurement

--- services/express.js
+++ services/express.js
@@ -3,10 +3,13 @@ import Utils from "../utils/index.js";
 import Routes from "../routes/index.js";
 import path from "path";
 import session from "express-session";
+import helmet from "helmet";
 
 class _ExpressService {
     initialBackend() {
         let app = express();
+        app.use(helmet())
+        app.disable('x-powered-by')
         app.set('views', path.join(path.resolve("./"), '/views'));
         app.set('view engine', 'ejs');
         app.use(session({
@@ -19,6 +22,15 @@ class _ExpressService {
         app.use(express.urlencoded({extended: false}));
         app.use("/", express.static(path.join(path.resolve("./"), '/public')));
         Routes.setRoutes(app);
+        // custom 404
+        app.use((req, res, next) => {
+            res.status(404).send("Sorry can't find that!")
+        })
+        // custom error handler
+        app.use((err, req, res, next) => {
+            console.error(err.stack)
+            res.status(500).send('Something broke!')
+        })
         let server = app.listen(process.env.SERVER_BACKEND_PORT);
         Utils.Logger.printInfo('Safety Controller Backend Service Started at: ' + 'http://localhost:' + process.env.SERVER_BACKEND_PORT);
     }
